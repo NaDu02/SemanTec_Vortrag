3a16bc214ae466ffa74a2a5abd25aaab2f230bc9
Linus <58478014+NaDu02@users.noreply.github.com>
2024-01-22 16:53:01 +0100

    Add todo list to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { HeaderComponent } from "./header/header.component";
+import { TodoTitleComponent } from './todo-title/todo-title.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ProfileComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, ProfileComponent, HeaderComponent, TodoTitleComponent, CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
